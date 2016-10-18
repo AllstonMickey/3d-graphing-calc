2780266b8ad8a26cdd3a9566e9ea63a36805f318
kaiergin <kergin2016@gmail.com>
2016-10-18 16:35:47 -0700

    Added method to find intersection of axes

--- Graph.java
+++ Graph.java
@@ -24,6 +24,7 @@ class Graph{
 		double d = (-1)*fx*x + (-1)*fy*y + z;
 		double[] normVector = {-fx,-fy,1};
 		Line[][] vectors = new Line[500][500];
+		int count = 0;
 		for(int a=0;a<500;a++){
 			for(int b=0;b<500;b++){
 				double i = ((double)(a-250)/(double)(500 * Math.sqrt(1+fx*fx)))+x;
@@ -32,7 +33,21 @@ class Graph{
 				double[] arg = {i,j,k};
 				vectors[a][b] = new Line(normVector,arg);
 				//System.out.println(vectors[a][b].pVector[0] + " " + vectors[a][b].pVector[1] + " " + vectors[a][b].pVector[2]);
+				if(placeAxis(vectors[a][b])){
+					count++;
+				}
 			}
 		}
+		System.out.println(count);
+	}
+	public static boolean placeAxis(Line check){
+		if((-1*check.pVector[0]/check.dVector[0]) - 0.001  < (-1*check.pVector[1]/check.dVector[1]) && (-1*check.pVector[0]/check.dVector[0]) + 0.001  >= (-1*check.pVector[1]/check.dVector[1])){
+			return true;
+		} else if((-1*check.pVector[0]/check.dVector[0]) - 0.001  < (-1*check.pVector[2]/check.dVector[2]) && (-1*check.pVector[0]/check.dVector[0]) + 0.001  >= (-1*check.pVector[2]/check.dVector[2])){
+			return true;
+		} else if((-1*check.pVector[2]/check.dVector[2]) - 0.001  < (-1*check.pVector[1]/check.dVector[1]) && (-1*check.pVector[2]/check.dVector[2]) + 0.001  >= (-1*check.pVector[1]/check.dVector[1])){
+			return true;
+		}
+		return false;
 	}
 }
