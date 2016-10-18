1375b97ba57a39b22e94b31ef1d6d4aeb41ab0d8
kaiergin <kergin2016@gmail.com>
2016-10-18 09:57:35 -0700

    First commit

---
+++ Graph.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+import java.awt.Graphics;
+import java.lang.Math;
+
+class Graph{
+	public static void main(String[] args){
+		double x = Double.parseDouble(args[0]);
+		double y = Double.parseDouble(args[1]);
+		if(Math.sqrt(x*x+y*y)>=1){
+			System.out.println("Error in your numbers");
+			return;
+		}
+		boolean zside = Boolean.parseBoolean(args[2]);
+		double z = Math.sqrt(1-x*x-y*y);
+		double fx = -x/Math.sqrt(1-x*x-y*y);
+		double fy = -y/Math.sqrt(1-x*x-y*y);
+		if(zside){
+			z*=-1;
+			fx*=-1;
+			fy*=-1;
+			System.out.println("You chose the lower half of the sphere");
+		}
+		double d = (-1)*fx*x + (-1)*fy*y + z;
+		double[] normVector = {-fx,-fy,1};
+		Line[][] vectors = new Line[500][500];
+		for(int a=0;a<500;a++){
+			for(int b=0;b<500;b++){
+				double i = ((double)(a-250)/(double)(500 * Math.sqrt(1+fx*fx)))+x;
+				double j = ((double)(b-250)/(double)(500 * Math.sqrt(1+fy*fy)))+y;
+				double k = d + fx*i + fy*j;
+				double[] arg = {i,j,k};
+				vectors[a][b] = new Line(normVector,arg);
+				//System.out.println(vectors[a][b].pVector[0] + " " + vectors[a][b].pVector[1] + " " + vectors[a][b].pVector[2]);
+			}
+		}
+	}
+}
