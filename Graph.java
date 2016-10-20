d21ee788f2d2335ca2ebfa2c219e06fd5ee88059
kaiergin <kergin2016@gmail.com>
2016-10-20 16:50:34 -0700

    Created method for line array. Bug found when x or y = 0

--- Graph.java
+++ Graph.java
@@ -7,11 +7,27 @@ class Graph{
 	public static void main(String[] args){
 		double x = Double.parseDouble(args[0]);
 		double y = Double.parseDouble(args[1]);
+		boolean zside = Boolean.parseBoolean(args[2]);
+		Line[][] myLines = genScreen(x,y,zside);
+	}
+	public static boolean placeAxis(Line check){
+		double a = -1*check.pVector[0]/check.dVector[0];
+		double b = -1*check.pVector[1]/check.dVector[1];
+		double c = -1*check.pVector[2]/check.dVector[2];
+		if(Math.abs(a-b)<=.001){
+			return true;
+		} else if(Math.abs(b-c)<=.001){
+			return true;
+		} else if(Math.abs(c-a)<=.001){
+			return true;
+		}
+		return false;
+	}
+	public static Line[][] genScreen(double x, double y, boolean zside){
 		if(Math.sqrt(x*x+y*y)>=1){
 			System.out.println("Error in your numbers");
-			return;
+			System.exit(0);
 		}
-		boolean zside = Boolean.parseBoolean(args[2]);
 		double z = Math.sqrt(1-x*x-y*y);
 		double fx = -x/Math.sqrt(1-x*x-y*y);
 		double fy = -y/Math.sqrt(1-x*x-y*y);
@@ -35,19 +51,13 @@ class Graph{
 				//System.out.println(vectors[a][b].pVector[0] + " " + vectors[a][b].pVector[1] + " " + vectors[a][b].pVector[2]);
 				if(placeAxis(vectors[a][b])){
 					count++;
+					System.out.println(vectors[a][b].pVector[0] + " " + vectors[a][b].pVector[1] + " " + vectors[a][b].pVector[2]);
+					System.out.println(a + " " + b);
 				}
 			}
 		}
+		System.out.println(vectors[0][0].dVector[0] + " " + vectors[0][0].dVector[1] + " " + vectors[0][0].dVector[2]);
 		System.out.println(count);
-	}
-	public static boolean placeAxis(Line check){
-		if((-1*check.pVector[0]/check.dVector[0]) - 0.001  < (-1*check.pVector[1]/check.dVector[1]) && (-1*check.pVector[0]/check.dVector[0]) + 0.001  >= (-1*check.pVector[1]/check.dVector[1])){
-			return true;
-		} else if((-1*check.pVector[0]/check.dVector[0]) - 0.001  < (-1*check.pVector[2]/check.dVector[2]) && (-1*check.pVector[0]/check.dVector[0]) + 0.001  >= (-1*check.pVector[2]/check.dVector[2])){
-			return true;
-		} else if((-1*check.pVector[2]/check.dVector[2]) - 0.001  < (-1*check.pVector[1]/check.dVector[1]) && (-1*check.pVector[2]/check.dVector[2]) + 0.001  >= (-1*check.pVector[1]/check.dVector[1])){
-			return true;
-		}
-		return false;
+		return vectors;
 	}
 }
